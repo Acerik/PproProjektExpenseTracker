1feaec699068efbeba0d7fae1fff0dabdb8606c2
Acerik <vana.mat@seznam.cz>
2024-12-14 22:00:47 +0100

    Add global advice model attribute

---
+++ src/main/java/cz/uhk/pproprojektexpensetracker/controller/GlobalControllerAdvice.java
@@ -0,0 +1,19 @@
+package cz.uhk.pproprojektexpensetracker.controller;
+
+import cz.uhk.pproprojektexpensetracker.model.User;
+import cz.uhk.pproprojektexpensetracker.model.UserRole;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ModelAttribute;
+
+@ControllerAdvice
+public class GlobalControllerAdvice {
+
+
+    @ModelAttribute
+    public void addAttributes(Model model, @AuthenticationPrincipal User user) {
+        model.addAttribute("isAdmin", user != null
+                && user.getUserRoles().contains(UserRole.ADMIN));
+    }
+}
